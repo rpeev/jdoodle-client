87a57f0f5db64985d664b689ce54ef7a05b10b8d
Radoslav Peev <rpeev@ymail.com>
2019-04-25 23:00:35 +0300

    Use explicit .mjs ext in example

--- example/jdoodle-client.node.mjs
+++ example/jdoodle-client.node.mjs
@@ -1,4 +1,4 @@
-import jdoodle from '../dist/jdoodle-client.node';
-//import jdoodle from 'jdoodle-client/dist/jdoodle-client.node';
+import jdoodle from '../dist/jdoodle-client.node.mjs';
+//import jdoodle from 'jdoodle-client/dist/jdoodle-client.node.mjs';
 
 console.log(jdoodle);
