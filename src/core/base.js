f39eca2a990858592b1ac037dcef82affd7bafa9
Radoslav Peev <rpeev@ymail.com>
2021-10-16 10:57:30 +0300

    Reorder opts

--- src/core/base.js
+++ src/core/base.js
@@ -8,12 +8,12 @@ const jdoodle = {
   opts: {
     executeEndpoint: 'https://api.jdoodle.com/v1/execute',
     executePath: '/jdoodleExecute',
-    creditSpentEndpoint: 'https://api.jdoodle.com/v1/credit-spent',
-    creditSpentPath: '/jdoodleCreditSpent',
     language: 'ruby',
     versionIndex: 3,
     stdin: 'Hi',
-    script: 'puts "ruby #{RUBY_VERSION}: #{ARGF.read}"'
+    script: 'puts "ruby #{RUBY_VERSION}: #{ARGF.read}"',
+    creditSpentEndpoint: 'https://api.jdoodle.com/v1/credit-spent',
+    creditSpentPath: '/jdoodleCreditSpent'
   }
 };
 
