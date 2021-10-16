b79cf12cca3d131cd88666605e8541f71183e85f
Radoslav Peev <rpeev@ymail.com>
2021-10-16 10:46:56 +0300

    Simplify arrow func definitions

--- src/node/jdoodle-client.js
+++ src/node/jdoodle-client.js
@@ -1,31 +1,29 @@
 import {request} from 'https';
 import jdoodle from '../universal/base';
 
-const _api = (url, opts) => {
-  return new Promise((resolve, reject) => {
-    const json = JSON.stringify(opts);
-    const req = request(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': json.length
-      }
-    }, res => {
-      const buf = [];
-      res.
-        on('data', data => buf.push(String(data))).
-        on('end', () => {
-          // Wrap in a try-catch because JSON.parse might throw and
-          // we're in an unprotected callback after the promise is created
-          try { resolve(JSON.parse(buf.join(''))); }
-          catch (err) { reject(err); }
-        }).
-        on('error', err => reject(err));
-    }).on('error', err => reject(err));
-    req.write(json);
-    req.end();
-  });
-};
+const _api = (url, opts) => new Promise((resolve, reject) => {
+  const json = JSON.stringify(opts);
+  const req = request(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': json.length
+    }
+  }, res => {
+    const buf = [];
+    res.
+      on('data', data => buf.push(String(data))).
+      on('end', () => {
+        // Wrap in a try-catch because JSON.parse might throw and
+        // we're in an unprotected callback after the promise is created
+        try { resolve(JSON.parse(buf.join(''))); }
+        catch (err) { reject(err); }
+      }).
+      on('error', err => reject(err));
+  }).on('error', err => reject(err));
+  req.write(json);
+  req.end();
+});
 
 const rawExecute = ({
   endpoint = process.env.JDOODLE_ENDPOINT_EXECUTE || jdoodle.opts.executeEndpoint,
@@ -35,27 +33,23 @@ const rawExecute = ({
   versionIndex = jdoodle.opts.versionIndex,
   stdin = jdoodle.opts.stdin,
   script = jdoodle.opts.script
-} = {}) => {
-  return _api(endpoint, {
-    clientId,
-    clientSecret,
-    language,
-    versionIndex,
-    stdin,
-    script
-  });
-};
+} = {}) => _api(endpoint, {
+  clientId,
+  clientSecret,
+  language,
+  versionIndex,
+  stdin,
+  script
+});
 
 const rawCreditSpent = ({
   endpoint = process.env.JDOODLE_ENDPOINT_CREDIT_SPENT || jdoodle.opts.creditSpentEndpoint,
   clientId = process.env.JDOODLE_CLIENT_ID,
   clientSecret = process.env.JDOODLE_CLIENT_SECRET
-} = {}) => {
-  return _api(endpoint, {
-    clientId,
-    clientSecret
-  });
-};
+} = {}) => _api(endpoint, {
+  clientId,
+  clientSecret
+});
 
 const execute = opts => callExecuteAPI(opts).
   catch(err => ({error: `${err}`, statusCode: 500}));
