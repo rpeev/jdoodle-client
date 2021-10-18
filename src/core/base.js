f130ccd08b448fddc2d69d4804af33f10c71a23a
Radoslav Peev <rpeev@ymail.com>
2021-10-18 20:59:02 +0300

    Use named imports from package.json

--- src/core/base.js
+++ src/core/base.js
@@ -1,8 +1,8 @@
-import pkg from '../../package.json';
+import {browserGlobal, version} from '../../package.json';
 
 const jdoodle = {
-  get [Symbol.toStringTag]() { return pkg.browserGlobal; },
-  version: pkg.version,
+  get [Symbol.toStringTag]() { return browserGlobal; },
+  version: version,
   opts: {
     executeEndpoint: 'https://api.jdoodle.com/v1/execute',
     executePath: '/jdoodleExecute',
