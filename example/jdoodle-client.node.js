9aeef22ee998951afc83cc251f234fff56659b1a
Radoslav Peev <rpeev@ymail.com>
2021-10-23 11:45:51 +0300

    Update examples

--- example/jdoodle-client.node.js
+++ example/jdoodle-client.node.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 'use strict';
 
+const {config} = require('dotenv');
 const jdoodle = require('jdoodle-client');
 
 console.log(jdoodle);
+
+(async () => {
+  config();
+
+  console.log(await jdoodle.execute());
+  console.log(await jdoodle.creditSpent());
+})();
--- example/jdoodle-client.node.mjs
+++ example/jdoodle-client.node.mjs
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+import {config} from 'dotenv';
 import jdoodle from 'jdoodle-client';
 
 console.log(jdoodle);
+
+config();
+
+console.log(await jdoodle.execute());
+console.log(await jdoodle.creditSpent());
