c5434799eed5845079404088d15091e65af8814e
Radoslav Peev <rpeev@ymail.com>
2021-10-25 21:04:11 +0300

    Move defaults property definition

--- src/core/jdoodle-client.mjs
+++ src/core/jdoodle-client.mjs
@@ -1,18 +1,20 @@
 import {browserGlobal, version} from '../../package.json';
 
+const defaults = {
+  executeEndpoint: 'https://api.jdoodle.com/v1/execute',
+  executePath: '/jdoodleExecute',
+  language: 'ruby',
+  versionIndex: 0,
+  stdin: '',
+  script: 'puts "(ruby #{RUBY_VERSION}) Hi"',
+  creditSpentEndpoint: 'https://api.jdoodle.com/v1/credit-spent',
+  creditSpentPath: '/jdoodleCreditSpent'
+};
+
 const jdoodle = {
   get [Symbol.toStringTag]() { return browserGlobal; },
   version: version,
-  defaults: {
-    executeEndpoint: 'https://api.jdoodle.com/v1/execute',
-    executePath: '/jdoodleExecute',
-    language: 'ruby',
-    versionIndex: 0,
-    stdin: '',
-    script: 'puts "(ruby #{RUBY_VERSION}) Hi"',
-    creditSpentEndpoint: 'https://api.jdoodle.com/v1/credit-spent',
-    creditSpentPath: '/jdoodleCreditSpent'
-  }
+  defaults
 };
 
 export default jdoodle;
